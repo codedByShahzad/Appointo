06f80379c7d6ac72b60c4aae8ec3c0dc4a53b63b
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2025-11-25 22:08:22 +0500

    All Doctors Api Completed

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import {addDoctor, adminLogin} from '../controllers/adminController.js'
+import {addDoctor, adminLogin, allDoctors} from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import { autAdmin } from '../middlewares/authAdmin.js'
 
 const adminRouter = express.Router()
 
 adminRouter.post("/add-doctor", autAdmin ,upload.single('image') ,addDoctor)
+adminRouter.post("/all-doctors", autAdmin, allDoctors)
 adminRouter.post("/login", adminLogin)
 
 
